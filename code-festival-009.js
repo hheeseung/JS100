e112156d6e98f0266bdabc47806441c3110845be
hheeseung <ssigu69@gmail.com>
2022-05-07 20:49:55 +0900

    solved 009

---
+++ code-festival-009.js
@@ -0,0 +1,25 @@
+// 다음 소스코드를 완성하여 날짜와 시간을 출력하시오
+
+const year = "2019";
+const month = "04";
+const day = "26";
+const hour = "11";
+const minute = "34";
+const second = "27";
+
+// 빈칸을 채워주세요
+const result = year.concat(
+  "/",
+  month,
+  "/",
+  day,
+  " ",
+  hour,
+  ":",
+  minute,
+  ":",
+  second
+);
+
+// 출력: 2019/04/26 11:34:27
+console.log(result);
