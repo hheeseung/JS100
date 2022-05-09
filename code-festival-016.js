41a13b460a3fd8f727993fa7dbca413d1f6bfa5e
hheeseung <ssigu69@gmail.com>
2022-05-09 21:51:44 +0900

    code refactored

--- code-festival-016.js
+++ code-festival-016.js
@@ -1,8 +1,7 @@
 /**
  * 문장이 입력되면 거꾸로 출력하는 프로그램을 만들어봅시다.
  */
-let sentence = prompt("문장을 입력해주세요.");
-sentence.split("");
+const sentence = prompt("문장을 입력해주세요.").split("");
 let reverseSentence = [];
 for (let i = 0; i < sentence.length; i++) {
   reverseSentence.unshift(sentence[i]);
